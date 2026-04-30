7405133f5d7cf4dbefc102f4bbe5c4e6e8f245dc
4ipset <129294049+4ipset111@users.noreply.github.com>
2026-04-30 20:14:09 +0400

    Update editor_entity.h

--- src/editor/editor_entity.h
+++ src/editor/editor_entity.h
@@ -3,8 +3,6 @@
 #include "../headers/scene.h"
 #include "../headers/entity.h"
 
-// Entity creation
 Entity make_entity_from_asset(Scene& scene, ModelAsset& asset);
 
-// Entity naming
 void assign_entity_name(Entity& entity, const char* new_name);
